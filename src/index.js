53a8e1ed1e4c15b270addda6aed9b2aa8bf07ecd
Chris Searle <chris@chrissearle.org>
2016-11-16 19:40:30 +0100

    Allow for redux chrome dev

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const reducer = combineReducers({
     routing: routerReducer
 })
 
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const history = syncHistoryWithStore(browserHistory, store)
 
